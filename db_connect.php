2351a8a297d71cf795c5687ae0c3a7eaded6d140
joulebuencido <joule.louis.buencido@gmail.com>
2025-07-04 21:22:06 +0800

    ops: changed the connection pointer to access local version

--- db_connect.php
+++ db_connect.php
@@ -2,17 +2,17 @@
 
 // Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
+// $cleardb_server = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 
 
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db) or die("Could not connect to mysql" . mysqli_connect_error());
+// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db) or die("Could not connect to mysql" . mysqli_connect_error());
 
-// $conn = mysqli_connect('localhost','root','','loan_db') or die("Could not connect to mysql" . mysqli_connect_error());
+$conn = mysqli_connect('localhost','root','','loan_db') or die("Could not connect to mysql" . mysqli_connect_error());
 
 // $conn= new mysqli('localhost','root','','loan_db')or die("Could not connect to mysql".mysqli_error($con));
